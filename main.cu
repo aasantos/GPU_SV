b236543108950a30bdf0b629dc270261665b7884
aasantos <aasantos@fe.uc.pt>
2022-01-13 11:43:08 +0000

    Create main.cu

---
+++ main.cu
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <cuda.h>
+
+
+int main()
+{
+  printf("Hello from GPU_DLM\n");
+  return 0;
+}
