a556b527986bed52d909836e76fb563eafbecb78
aasantos <aasantos@fe.uc.pt>
2022-01-14 11:02:37 +0000

    Update main.cu

--- main.cu
+++ main.cu
@@ -5,8 +5,10 @@
 #include <string.h>
 #include <cuda.h>
 #include "io.cuh"
+#include "vector.cuh"
 #include "random.cuh"
 #include "normalmodel.cuh"
+#include "regmodel.cuh"
 #include "ar1model.cuh"
 #include "dlmmodel.cuh"
 
