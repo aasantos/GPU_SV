b595c56376570be9c7f6c5260b6db84d555cb624
aasantos <aasantos@fe.uc.pt>
2022-01-23 10:22:38 +0000

    Update svl.cuh

--- svl.cuh
+++ svl.cuh
@@ -1,6 +1,17 @@
 #ifndef svl_cuh
 #define svl_cuh
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <cuda.h>
+#include "vector.cuh"
+#include "random.cuh"
+#include "normalmodel.cuh"
+#include "regmodel.cuh"
+#include "ar1model.cuh"
+
 
 class SVLModel{
 
