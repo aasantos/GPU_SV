5dfafa961d26504d64fb86becffd39d26cc03e84
aasantos <aasantos@fe.uc.pt>
2022-01-31 15:46:51 +0000

    Update runfunc.cuh

--- runfunc.cuh
+++ runfunc.cuh
@@ -19,50 +19,13 @@
 #include "svt.cuh"
 #include "svl.cuh"
 #include "svtl.cuh"
-
-__global__ void estimate_sv_sp500_gpu(float *x,int n,float *musimul,float *phisimul,float *sigmasimul,float *rhosimul,unsigned int *seed,int niter)
-{
-  int idx = blockDim.x*blockIdx.x + threadIdx.x;
-  if(idx < niter){
-    //
-    float mut = -0.5;
-    float phit = 0.97;
-    float sigmat = 0.2;
-    float rhot = -0.7;
-    //
-    int nwarmup = 1000;
-    //
-    SVLModel<float> *model = new SVLModel<float>(x,n,mut,phit,sigmat,rhot);
-    model->setseed(seed[idx]);
-    //
-    for(int i=0;i<100;i++){
-        model->simulatestates();
-    }
-    //
-    // 
-    for(int i=0;i<nwarmup;i++){
-        model->simulatestates();
-        model->simulatemu();
-        model->simulatephi();
-        model->simulatesigmarho();
-    }
-    //
-    musimul[idx] = model->simulatemu();
-    phisimul[idx] = model->simulatephi();
-    model->simulatesigmarho();
-    sigmasimul[idx] = model->getsigma();
-    rhosimul[idx] = model->getrho();
-    //
-    delete model;
-    //
-  }// if(idx < niter)
-}
+#include "kernelfunc.cuh"
 
 void estimate_gpu_sv_sp500()
 {
   printf("Start estimating .... \n");
   int n;
-  float *xi = readArray<float>("sp500y.txt",&n);
+  float *xi = readArray<float>("sp500_ret_80_87.txt",&n);
   //
   float *x;
   cudaMallocManaged(&x,n*sizeof(float));
