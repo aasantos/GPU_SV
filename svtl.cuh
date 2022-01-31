03bc06624dc2c24476978b20a50abfb884a5bab7
aasantos <aasantos@fe.uc.pt>
2022-01-31 15:36:42 +0000

    Update svtl.cuh

--- svtl.cuh
+++ svtl.cuh
@@ -102,25 +102,28 @@ public:
     //
     __host__ __device__ T df(T yy,T yy0,T a0,T a,T a1)
     {
-        T t1 = -0.5e0 + 0.5e0 * yy * yy * exp(-a);
-        T t2 = -(a1 - this->mu - this->phi * (a - this->mu) - this->sigma
-                 * this->rho * exp(-0.5e0 * a) * yy) * pow(this->sigma, -0.2e1) /
-        (-this->rho * this->rho + 0.1e1) * (-this->phi + 0.5e0 *
-                                            this->sigma * this->rho * exp(-0.5e0 * a) * yy);
-        T t3 = -(a - this->mu - this->phi * (a0 - this->mu) - this->sigma *
-                 this->rho * exp(-0.5e0 * a0) * yy0) * pow(this->sigma, -0.2e1) / (-this->rho * this->rho + 0.1e1);
+        T sigmat = this->sigma*sqrtf(1.0 - this->rho*this->rho);
+        T sigmasq = sigmat*sigmat;
+        T tt = this->sigma*this->rho*expf(-0.5*a)*yy;
+        T t1 = -0.5 + 0.5*yy*yy*expf(-1.0*a);
+        T err0 = 0.5*this->sigma*tt - this->phi;
+        T err1 = (a1 - this->mu - this->phi*(a - this->mu) - tt);
+        T t2 = -1.0*err0*err1/sigmasq;
+        T err2 = (a - this->mu - this->phi*(a0 - this->mu) - this->sigma*this->rho*expf(-0.5*a0)*yy0);
+        T t3 = -1.0*err2/sigmasq;
         return t1 + t2 + t3;
     }
     //
-    __host__ __device__ T ddf(T yy,T a)
+    __host__ __device__ T ddf(T yy,T a,T a1)
     {
-        T t1 = -0.5e0 * yy * yy * exp(-a);
-        T t2 = -pow(-this->phi + 0.5e0 * this->sigma * this->rho * exp(-0.5e0 * a) * yy, 0.2e1) *
-        pow(this->sigma, -0.2e1) / (-this->rho * this->rho + 0.1e1)
-        + 0.25e0 * (a1 - this->mu - this->phi * (a - this->mu) - this->sigma *
-                    this->rho * exp(-0.5e0 * a) * yy) / this->sigma /
-        (-this->rho * this->rho + 0.1e1) * this->rho * exp(-0.5e0 * a) * yy;
-        T t3 = -pow(this->sigma, -0.2e1) / (double) (-this->rho * this->rho + 1.0);
+        T sigmat = this->sigma*sqrtf(1.0 - this->rho*this->rho);
+        T sigmasq = sigmat*sigmat;
+        T tt = this->rho*expf(-0.5*a)*yy;
+        T t1 = -0.5*yy*yy*expf(-1.0*a);
+        T err0 = 0.5*this->sigma*tt - this->phi;
+        T err1 = (a1 - this->mu - this->phi*(a - this->mu) - this->sigma*tt);
+        T t2 = -1.0*err0*err0/sigmasq + 0.25*err1*tt/(sigmasq/this->sigma);
+        T t3 = -1.0/sigmasq;
         return t1 + t2 + t3;
     }
     //
@@ -130,7 +133,7 @@ public:
         T g = df(yy,yy0,a0,x0,a1);
         int iter = 0;
         while(fabs(g) > 0.00001 && iter < 20){
-            T h = ddf(yy,x0);
+            T h = ddf(yy,x0,a1);
             x0 = x0 - g/h;
             g = df(yy,yy0,a0,x0,a1);
             iter++;
@@ -143,23 +146,21 @@ public:
         return newton(yy,yy0,a0,a1);
     }
     //
-    __host__ __device__ T stdstate(T yy,T a)
+    __host__ __device__ T stdstate(T yy,T a,T a1)
     {
-        return sqrt(-1.0/ddf(yy,a));
+        return sqrt(-1.0/ddf(yy,a,a1));
     }
     //
     __host__ __device__ T loglik(T yy,T yy0,T a0,T a,T a1)
     {
-        T t1 = -0.5e0 * a - 0.5e0 * yy * yy * exp(-a);
-        T t2 = -pow(a1 - this->mu - this->phi * (a - this->mu) - this->sigma
-                    * this->rho * exp(-0.5e0 * a) * yy, 0.2e1) * pow(this->sigma, -0.2e1)
-        / (-this->rho * this->rho + 0.1e1) / 0.2e1;
-        T t3 = -pow(a - this->mu - this->phi * (a0 - this->mu) - this->sigma *
-                    this->rho * exp(-0.5e0 * a0) * yy0, 0.2e1) * pow(this->sigma, -0.2e1)
-        / (-this->rho * this->rho + 0.1e1) / 0.2e1;
+        T sigmat = this->sigma*sqrtf(1.0 - this->rho*this->rho);
+        T t1 = -0.5*a - 0.5*yy*yy*expf(-1.0*a);
+        T err1 = (a1 - this->mu - this->phi*(a - this->mu) - this->sigma*this->rho*expf(-0.5*a)*yy);
+        T t2 = -0.5*err1*err1/(sigmat*sigmat);
+        T err2 = (a - this->mu - this->phi*(a0 - this->mu) - this->sigma*this->rho*expf(-0.5*a0)*yy0);
+        T t3 = -0.5*err2*err2/(sigmat*sigmat);
         return t1 + t2 + t3;
-    }
-    //
+    }    //
     //
     __host__ __device__ T lognorm(T a,T m,T s)
     {
@@ -196,7 +197,7 @@ public:
                 this->a1 = this->mu + this->phi*(this->alpha[i] - this->mu)
                 + this->random->normal(0.0,this->sigma);
                 T mx = meanstate(yy,yy0,a0,a1);
-                T sx = stdstate(yy,mx);
+                T sx = stdstate(yy,mx,a1);
                 T atemp = this->random->normal(mx, sx);
                 if(this->random->uniform() < metroprob(atemp,this->alpha[i],a0,a1,yy,yy0,mx,sx)){
                     this->alpha[i] = atemp;
@@ -207,7 +208,7 @@ public:
                 this->a0 = this->alpha[i-1];
                 this->a1 = this->alpha[i+1];
                 T mx = meanstate(yy,yy0,a0,a1);
-                T sx = stdstate(yy,mx);
+                T sx = stdstate(yy,mx,a1);
                 T atemp = this->random->normal(mx, sx);
                 if(this->random->uniform() < metroprob(atemp,this->alpha[i],a0,a1,yy,yy0,mx,sx)){
                     this->alpha[i] = atemp;
@@ -335,12 +336,6 @@ public:
     }
     //
     //
-    //void setsigmadiffuse(bool sdiffuse)
-    //{
-    //    this->sigmadiffuse = sdiffuse;
-    //}
-    //
-    //
     __host__ __device__ void setmuprior(T mprior[2])
     {
         this->muprior[0] = mprior[0];
@@ -353,13 +348,6 @@ public:
         this->phiprior[0] = pprior[0];
         this->phiprior[1] = pprior[1];
     }
-    //
-    //
-    //void setsigmaprior(T sprior[2])
-    //{
-    //    this->sigmaprior[0] = sprior[0];
-    //    this->sigmaprior[1] = sprior[1];
-    //}
     //
     //
     __host__ __device__ void setphipriortype(int t)
@@ -380,5 +368,6 @@ public:
     //
     
 };
+
 
 #endif
