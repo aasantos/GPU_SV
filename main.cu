9bf95431240fe1394ab872fee40f0bb32b144b68
António Alberto Santos <aasantos@fe.uc.pt>
2022-02-19 13:43:24 +0000

    Update main.cu

--- main.cu
+++ main.cu
@@ -16,7 +16,7 @@
 //
 //
 //
-int main()
+int main(int argc,char **argv)
 {
   printf("Start .... \n");
   //
@@ -24,10 +24,11 @@ int main()
   //estimate_svt("sp500_ret_80_87.txt",-0.5,0.95,0.2,20);
   //estimate_svl("sp500_ret_80_87.txt",-0.5,0.95,0.2,-0.5);
   //estimate_svtl("sp500_ret_80_87.txt",-0.5,0.95,0.2,-0.5,20);
-  //estimate_sv_gpu("sp500_ret_80_87.txt");
-  estimate_svl_gpu("sp500_ret_80_87.txt");
-  //estimate_svtl_gpu("sp500_ret_80_87.txt");
-  //estimate_svt_gpu("sp500_ret_80_87.txt");
+  int flag = atoi(argv[1]);
+  if(flag == 0) estimate_sv_gpu("sp500_ret_80_87.txt");
+  if(flag == 1) estimate_svl_gpu("sp500_ret_80_87.txt");
+  if(flag == 2) estimate_svtl_gpu("sp500_ret_80_87.txt");
+  if(flag == 3) estimate_svt_gpu("sp500_ret_80_87.txt");
   //
   //
   printf("Done ... \n");
